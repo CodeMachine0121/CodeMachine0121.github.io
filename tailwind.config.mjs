8a74115c77d279f085965c274b61a9eb42a9099f
薛華慶, james.hsueh <james.hsueh@miniasp.com>
2026-01-29 01:24:52 +0800

    feat: enhance portfolio section style and improve content consistency

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				heading: ['Poppins', 'sans-serif'],
+				body: ['Open Sans', 'sans-serif'],
+				sans: ['Open Sans', 'sans-serif'],
+			},
 			colors: {
 				primary: "var(--color-primary)",
 				secondary: "var(--color-secondary)",
